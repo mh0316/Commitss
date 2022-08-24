bee9171f427e76e270d890051998318bc202ce5f
Marcelo <m.henriquez11@ufromail.cl>
2022-08-23 22:29:04 -0400

    Primer commit

---
+++ src/main/java/Ingreso.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ingreso {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner((System.in));
+        System.out.println("Ingrese un número:");
+        int numero = leer.nextInt();
+    }
+}
